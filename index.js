73ea3599a13ca835d0aa12a46107241ae9f469ad
oknp <oknp.gidwai@gmail.com>
2015-12-19 13:24:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
   $(window).bind('hashchange',function on_hashchange() {
     var route = location.hash.substr(1);
     var template_route = route + '.html';
-    var 404_route = '/404.html';
+    var _404_route = '/404.html';
     $.get(template_route,function on_template_route_response(data) {
       $('#viewport').html(data);
     }).fail(function on_template_route_fail(fail) {
-      $.get(404_route),function on_404_route_response(data) {
+      $.get(_404_route),function on_404_route_response(data) {
         $('#viewport').html(data);
       });
     });
