aa9ea7765ef6bf126db2531cceee0508fec187e2
oknp <oknp.gidwai@gmail.com>
2015-12-19 13:30:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function($){
   $(window).bind('hashchange',function on_hashchange() {
     var route = location.hash.substr(1);
-    var template_route = route + '.html';
+    var template_route = 'views' + route + '.html';
     var _404_route = '/404.html';
     $.get(template_route,function on_template_route_response(data) {
       $('#viewport').html(data);
