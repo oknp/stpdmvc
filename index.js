3098bc899c69fdfa52c4cda8368cc2ab15f01f58
oknp <oknp.gidwai@gmail.com>
2015-12-19 13:19:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 (function($){
-  var app = $('#app');
+  $(window).bind('hashchange',function on_hashchange() {
+    var route = location.hash.substr(1);
+    var template_route = route + '.html';
+    $.get(template_route,function on_template_route_response(data) {
+      $('#viewport').html(data);
+    });
+  });
 })(jQuery);
