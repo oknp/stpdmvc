cf7c1e3d6fb07d860802256e5816c79c5ed3293c
Braun Goodson <braun.goodson@digitaslbi.com>
2015-12-20 13:43:36 -0500

    updated

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 (function($){
+
+  // data stub
   var oknp = {
     posts: [{
       title: 'pseudo post',
@@ -6,8 +8,14 @@
       body: 'pseudo body'
     }]
   };
+
+  // init
+  $(on_hashchange);
+
   // routing
-  $(window).bind('hashchange',function on_hashchange() {
+  $(window).bind('hashchange',on_hashchange);
+
+  function on_hashchange() {
     var route = get_route();
     var template_route = 'views' + route + '.html';
     var _404_route = '/404.html';
@@ -17,21 +25,27 @@
     }).fail(function on_template_route_fail(fail) {
       $.get(_404_route,function on_404_route_response(data) {
         $('#viewport').html(data);
+        update(get_route(),oknp);
       });
     });
-  }); // end-routing
+  }
+
   // data
   var fire = new Firebase('https://oknp.firebaseio-demo.com/');
-  fire.on('child_added', function fire_on_child_added(snapshot) {
+  fire.on('child_added',fire_on_child_added);
+
+  function fire_on_child_added(snapshot) {
     var val = snapshot.val();
     oknp = val ? oknp : val;
     var route = get_route();
     update(route,oknp);
-  }); // end-data
+  }
+
   // helpers
   function get_route() {
     return location.hash.substr(1);
   }
+
   function update(route,oknp) {
     switch (route) {
       case '/posts': update_posts(oknp.posts); break;
@@ -40,17 +54,19 @@
       default: break;
     }
   }
+
   function update_posts(posts_data) {
     var post_template_route = 'views/post.html';
     $.get(post_template_route,function on_post_template_route_response(post_template) {
-      var doms = '';
+      var posts = '';
       for (var p in posts_data) {
-        var dom_tree = post_template;
-        $('.post.title',dom_tree).text(posts_data[p].title);
-        $('.post.body',dom_tree).text(posts_data[p].body);
-        doms += dom_tree;
+        var post = $(post_template);
+        post.find('.title').text(posts_data[p].title);
+        post.find('.body').text(posts_data[p].body);
+        posts += $(post).html();
       }
-      $('#posts').append(doms);
+      $('#posts').html(posts);
     });
   }
+
 })(jQuery);
