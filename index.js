229cc390d50bdeabd0db8721e35c8b62d99a6ff1
Braun Goodson <braun.goodson@digitaslbi.com>
2015-12-19 23:11:29 -0500

    updated

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 (function($){
+  var oknp = {
+    posts: [{
+      title: 'pseudo post',
+      date: new Date().getTime(),
+      body: 'pseudo body'
+    }]
+  };
   // routing
   $(window).bind('hashchange',function on_hashchange() {
     var route = get_route();
@@ -6,6 +13,7 @@
     var _404_route = '/404.html';
     $.get(template_route,function on_template_route_response(data) {
       $('#viewport').html(data);
+      update(get_route(),oknp);
     }).fail(function on_template_route_fail(fail) {
       $.get(_404_route,function on_404_route_response(data) {
         $('#viewport').html(data);
@@ -15,7 +23,8 @@
   // data
   var fire = new Firebase('https://oknp.firebaseio-demo.com/');
   fire.on('child_added', function fire_on_child_added(snapshot) {
-    var oknp = snapshot.val();
+    var val = snapshot.val();
+    oknp = val ? oknp : val;
     var route = get_route();
     update(route,oknp);
   }); // end-data
@@ -34,9 +43,8 @@
   function update_posts(posts_data) {
     var post_template_route = 'views/post.html';
     $.get(post_template_route,function on_post_template_route_response(post_template) {
-      var posts_html = '';
+      console.log(post_template);
       for (var p in posts_data) {
-        posts_html += bind_post(post_template,posts[p]);
         $('.post.title',post_template).html(post_data.title);
         $('.post.body',post_template).html(post_data.body);
       }
